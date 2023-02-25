1464b2ef815b7ac1202d604b53cac82a1c9a88d0
MoniCamargo37 <moni28ac@gmail.com>
2023-02-25 11:50:46 +0100

    done

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Switch = () => {
+
+  const [darkCounter, setDarkCounter] = useState(0);
+  const [lightCounter, setLightCounter] = useState(0);
+  const [theme, setTheme] = useState('light');
+  
+  const handleLightTheme = () => {
+    setLightCounter(lightCounter + 1);
+    setTheme('light');
+  };
+
+  const handleDarkTheme = () =>  {
+    setTheme ('dark');
+    setDarkCounter(darkCounter +1);
+  };
   
   return (
-    <div className="switch light">
-      <h2>Theme picker</h2>
-      <h4>Times changed: </h4>
+    <div className={`switch ${theme}`}>
+    <div>
+      <h4>Light theme count: {lightCounter}</h4>
+      <button onClick={handleLightTheme}>Light theme</button>
     </div>
+    <div>
+      <h4>Dark theme count: {darkCounter}</h4>
+      <button onClick={handleDarkTheme}>Dark theme</button>
+    </div>
+  </div>
   )
 }
 
+
 export default Switch;
+
